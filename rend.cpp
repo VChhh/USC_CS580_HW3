e0cf4ec96ca7561fbf7298f7e98f64177782d2ad
VChhh <81150701+VChhh@users.noreply.github.com>
2024-09-29 23:11:31 -0700

    feat: implemented transformation --Nick

--- rend.cpp
+++ rend.cpp
@@ -7,8 +7,38 @@
 #include	"rend.h"
 
 #include "Vector3.h" // self-defined vector3 class
+#include "Vector4.h" // self-defined vector4 class
 
 #define PI (float) 3.14159265358979323846
+
+// matrix multiplication
+void matrix_multiply(GzMatrix mat1, GzMatrix mat2, GzMatrix result) {
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			float sum = 0;
+			for (size_t k = 0; k < 4; ++k) {
+				sum += mat1[i][k] * mat2[k][j];
+			}
+			result[i][j] = sum;
+		}
+	}
+}
+
+// transform the vector by the matrix
+template<typename T>
+Vector4<T> transform_vector4D(Vector4<T> vector, GzMatrix mat) {
+	Vector4<T> result;
+	for (size_t i = 0; i < 4; ++i) {
+		T sum = 0;
+		for (size_t j = 0; j < 4; ++j) {
+			sum += mat[i][j] * vector[j];
+		}
+		result[i] = sum;
+	}
+	return result;
+}
+
+
 
 int GzRender::GzRotXMat(float degree, GzMatrix mat)
 {
@@ -16,7 +46,19 @@ int GzRender::GzRotXMat(float degree, GzMatrix mat)
 // Create rotate matrix : rotate along x axis
 // Pass back the matrix using mat value
 */
-
+	float radian = degree * PI / 180.0f;
+	// initialize the matrix
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			mat[i][j] = 0;
+		}
+	}
+	mat[0][0] = 1.0f;
+	mat[1][1] = cos(radian);
+	mat[1][2] = -sin(radian);
+	mat[2][1] = sin(radian);
+	mat[2][2] = cos(radian);
+	mat[3][3] = 1.0f;
 	return GZ_SUCCESS;
 }
 
@@ -26,7 +68,19 @@ int GzRender::GzRotYMat(float degree, GzMatrix mat)
 // Create rotate matrix : rotate along y axis
 // Pass back the matrix using mat value
 */
-
+	float radian = degree * PI / 180.0f;
+	// initialize the matrix
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			mat[i][j] = 0;
+		}
+	}
+	mat[0][0] = cos(radian);
+	mat[0][2] = sin(radian);
+	mat[1][1] = 1.0f;
+	mat[2][0] = -sin(radian);
+	mat[2][2] = cos(radian);
+	mat[3][3] = 1.0f;
 	return GZ_SUCCESS;
 }
 
@@ -36,7 +90,19 @@ int GzRender::GzRotZMat(float degree, GzMatrix mat)
 // Create rotate matrix : rotate along z axis
 // Pass back the matrix using mat value
 */
-
+	float radian = degree * PI / 180.0f;
+	// initialize the matrix
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			mat[i][j] = 0;
+		}
+	}
+	mat[0][0] = cos(radian);
+	mat[0][1] = -sin(radian);
+	mat[1][0] = sin(radian);
+	mat[1][1] = cos(radian);
+	mat[2][2] = 1.0f;
+	mat[3][3] = 1.0f;
 	return GZ_SUCCESS;
 }
 
@@ -46,7 +112,19 @@ int GzRender::GzTrxMat(GzCoord translate, GzMatrix mat)
 // Create translation matrix
 // Pass back the matrix using mat value
 */
-
+	// initialize the matrix
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			mat[i][j] = 0;
+		}
+	}
+	mat[0][0] = 1.0f;
+	mat[1][1] = 1.0f;
+	mat[2][2] = 1.0f;
+	mat[3][3] = 1.0f;
+	mat[0][3] = translate[X];
+	mat[1][3] = translate[Y];
+	mat[2][3] = translate[Z];
 	return GZ_SUCCESS;
 }
 
@@ -57,7 +135,16 @@ int GzRender::GzScaleMat(GzCoord scale, GzMatrix mat)
 // Create scaling matrix
 // Pass back the matrix using mat value
 */
-
+	// initialize the matrix
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			mat[i][j] = 0;
+		}
+	}
+	mat[0][0] = scale[X];
+	mat[1][1] = scale[Y];
+	mat[2][2] = scale[Z];
+	mat[3][3] = 1.0f;
 	return GZ_SUCCESS;
 }
 
@@ -79,6 +166,34 @@ GzRender::GzRender(int xRes, int yRes)
 - setup Xsp and anything only done once 
 - init default camera 
 */ 
+	matlevel = -1;
+
+	// assign 0 to all Xsp elements
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			Xsp[i][j] = 0;
+		}
+	}
+
+	// set up Xsp
+	Xsp[0][0] = xres / 2.0f;
+	Xsp[1][1] = -yres / 2.0f;
+	Xsp[2][2] = MAXINT;
+	Xsp[0][3] = xres / 2.0f;
+	Xsp[1][3] = yres / 2.0f;
+	Xsp[3][3] = 1.0f;
+
+	// set up default camera
+	m_camera.position[X] = DEFAULT_IM_X;
+	m_camera.position[Y] = DEFAULT_IM_Y;
+	m_camera.position[Z] = DEFAULT_IM_Z;
+	m_camera.lookat[X] = 0.0f;
+	m_camera.lookat[Y] = 0.0f;
+	m_camera.lookat[Z] = 0.0f;
+	m_camera.worldup[X] = 0.0f;
+	m_camera.worldup[Y] = 1.0f;
+	m_camera.worldup[Z] = 0.0f;
+	m_camera.FOV = DEFAULT_FOV;
 }
 
 GzRender::~GzRender()
@@ -108,6 +223,69 @@ int GzRender::GzBeginRender()
 - init Ximage - put Xsp at base of stack, push on Xpi and Xiw 
 - now stack contains Xsw and app can push model Xforms when needed 
 */ 
+	// initialize Xpi and Xiw
+	GzMatrix Xpi;
+	GzMatrix Xiw;
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			Xpi[i][j] = 0;
+			Xiw[i][j] = 0;
+		}
+	}
+
+	// construct Xpi
+	// let dd = 1/d = tan(fov/2)
+	float dd = tan(m_camera.FOV * PI / 180.0f / 2.0f);
+	Xpi[0][0] = 1.0f;
+	Xpi[1][1] = 1.0f;
+	Xpi[2][2] = dd;
+	Xpi[3][2] = dd;
+	Xpi[3][3] = 1.0f;
+
+	// construct Xiw
+	// get the camera information in vector3f form
+	Vector3f vector_lookat = Vector3f(m_camera.lookat[X], m_camera.lookat[Y], m_camera.lookat[Z]);
+	Vector3f vector_position = Vector3f(m_camera.position[X], m_camera.position[Y], m_camera.position[Z]);
+	Vector3f vector_worldup = Vector3f(m_camera.worldup[X], m_camera.worldup[Y], m_camera.worldup[Z]);
+
+	// Z axis
+	Vector3f Z_axis = (vector_lookat - vector_position).normalize();
+
+	// up
+	Vector3f up_modified = vector_worldup - Z_axis * vector_worldup.dot(Z_axis);
+
+	// Y axis
+	Vector3f Y_axis = up_modified.normalize();
+
+	// X axis
+	Vector3f X_axis = Y_axis.cross(Z_axis);
+
+	Xiw[0][0] = X_axis[0];
+	Xiw[0][1] = X_axis[1];
+	Xiw[0][2] = X_axis[2];
+	Xiw[0][3] = -X_axis.dot(vector_position);
+	Xiw[1][0] = Y_axis[0];
+	Xiw[1][1] = Y_axis[1];
+	Xiw[1][2] = Y_axis[2];
+	Xiw[1][3] = -Y_axis.dot(vector_position);
+	Xiw[2][0] = Z_axis[0];
+	Xiw[2][1] = Z_axis[1];
+	Xiw[2][2] = Z_axis[2];
+	Xiw[2][3] = -Z_axis.dot(vector_position);
+	Xiw[3][3] = 1.0f;
+
+	//put Xpi and Xiw to the camera
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			m_camera.Xiw[i][j] = Xiw[i][j];
+			m_camera.Xpi[i][j] = Xpi[i][j];
+		}
+	}
+
+	// push into the stack
+	GzPushMatrix(Xsp);
+	GzPushMatrix(Xpi);
+	GzPushMatrix(Xiw);
 
 	return GZ_SUCCESS;
 }
@@ -117,7 +295,18 @@ int GzRender::GzPutCamera(GzCamera camera)
 /* HW 3.8 
 /*- overwrite renderer camera structure with new camera definition
 */
-
+	for (size_t i = 0; i < 4; ++i) {
+		for (size_t j = 0; j < 4; ++j) {
+			m_camera.Xiw[i][j] = camera.Xiw[i][j];
+			m_camera.Xpi[i][j] = camera.Xpi[i][j];
+		}
+	}
+	for (size_t i = 0; i < 3; ++i) {
+		m_camera.position[i] = camera.position[i];
+		m_camera.lookat[i] = camera.lookat[i];
+		m_camera.worldup[i] = camera.worldup[i];
+	}
+	m_camera.FOV = camera.FOV;
 	return GZ_SUCCESS;	
 }
 
@@ -127,7 +316,30 @@ int GzRender::GzPushMatrix(GzMatrix	matrix)
 - push a matrix onto the Ximage stack
 - check for stack overflow
 */
-	
+	// check for stack overflow
+	if (matlevel >= MATLEVELS) return GZ_FAILURE;
+
+	// push the matrix
+	if (matlevel == -1) {
+		// put the matrix at the base of the stack
+		for (size_t i = 0; i < 4; ++i) {
+			for (size_t j = 0; j < 4; ++j) {
+				Ximage[0][i][j] = matrix[i][j];
+			}
+		}
+	}
+	else {
+		// multiply the matrix with the top of the stack
+		GzMatrix result;
+		matrix_multiply(Ximage[matlevel], matrix, result);
+		for (size_t i = 0; i < 4; ++i) {
+			for (size_t j = 0; j < 4; ++j) {
+				Ximage[matlevel + 1][i][j] = result[i][j];
+			}
+		}
+	}
+	// increment the matlevel
+	++matlevel;
 	return GZ_SUCCESS;
 }
 
@@ -137,7 +349,9 @@ int GzRender::GzPopMatrix()
 - pop a matrix off the Ximage stack
 - check for stack underflow
 */
-
+	// check for stack underflow
+	if (matlevel < 0) return GZ_FAILURE;
+	--matlevel;
 	return GZ_SUCCESS;
 }
 
@@ -250,7 +464,20 @@ int GzRender::GzPutTriangle(int numParts, GzToken *nameList, GzPointer *valueLis
 	if (numParts <= 0 || nameList == NULL || valueList == NULL) return GZ_FAILURE;
 	for (size_t i = 0; i < numParts; ++i) {
 		if (nameList[i] == GZ_POSITION) {
-			Vector3f* vertices = (Vector3f*)valueList[i];
+			Vector3f* model_vertices = (Vector3f*)valueList[i];
+
+			Vector4f vertices_4D[3];
+			for (size_t j = 0; j < 3; ++j) {
+				vertices_4D[j] = Vector4f(model_vertices[j].x, model_vertices[j].y, model_vertices[j].z, 1.0f);
+				vertices_4D[j] = transform_vector4D(vertices_4D[j], Ximage[matlevel]);
+			}
+
+			if (vertices_4D[0].z < 0 || vertices_4D[1].z < 0 || vertices_4D[2].z < 0) return GZ_FAILURE;
+
+			Vector3f vertices[3];
+			for (size_t i = 0; i < 3; ++i) {
+				vertices[i] = vertices_4D[i].toVector3Homogeneous();
+			}
 
 			// sort by y
 			if (vertices[0].y > vertices[1].y) vertices[0].swap(vertices[1]);
